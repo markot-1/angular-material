5c6bfc57eee1c48a1ea27b848e0160b3b40bc148
Marina Kotova <m.klementyeva@mail.ru>
2025-02-21 19:17:38 +0700

    add dependencies for angular material, create subscribers component, change layout for widget and dashboard components

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Widget } from '../../configs/dashboard';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-widget',
-  imports: [],
+  imports: [MatButtonModule, MatRippleModule],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.scss'
 })
 export class WidgetComponent {
-
+  data = input.required<Widget>();
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { WidgetComponent } from "../../components/widget/widget.component";
+import { Widget } from '../../configs/dashboard';
+import { SubscribersComponent } from './widgets/subscribers/subscribers.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [WidgetComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  data: Widget = {
+    id: 1,
+    label: 'Subscribers',
+    content: SubscribersComponent,
+  }
 }
