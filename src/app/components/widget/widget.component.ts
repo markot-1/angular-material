211159c4a30ab28d5920e6fc43f9ee8dec0399e2
Marina Kotova <m.klementyeva@mail.ru>
2025-02-23 22:56:09 +0700

    change styles and layout in dashboard and widget components

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { Widget } from '../../configs/dashboard';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatRippleModule } from '@angular/material/core';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-widget',
-  imports: [MatButtonModule, MatRippleModule],
+  imports: [MatButtonModule, MatRippleModule, NgComponentOutlet],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.scss'
 })
