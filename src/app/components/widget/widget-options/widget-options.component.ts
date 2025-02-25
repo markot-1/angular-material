794714d415c49861ad10fb193a80fd800dc7ef18
Marina Kotova <m.klementyeva@mail.ru>
2025-02-25 20:22:50 +0700

    add styles and layout for widget-options component

--- src/app/components/widget/widget-options/widget-options.component.ts
+++ src/app/components/widget/widget-options/widget-options.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-widget-options',
-  imports: [],
+  imports: [MatButtonModule, MatIcon],
   templateUrl: './widget-options.component.html',
   styleUrl: './widget-options.component.scss'
 })
 export class WidgetOptionsComponent {
-
+  showOptions = model<boolean>(false)
 }
